e203183be4431a64539dd515a7452e92db1edd67
passing7by <passing7by.naver.com>
2025-06-19 17:15:22 +0900

    java 보충수업 - UUID

---
+++ chap14/src/sec03/exam04/quiz/Quiz1.java
@@ -0,0 +1,36 @@
+package sec03.exam04.quiz;
+
+import java.io.File;
+import java.util.UUID;
+
+public class Quiz1 {
+	// Quiz
+//	디렉터리 이름으로 UUID 사용하기
+//	임시 작업 디렉터리, 사용자 세션 디렉터리 등을 만들 때 UUID를 사용하면 
+//	충돌 없는 고유 디렉터리를 쉽게 만들 수 있습니다.
+	
+//	C:\test\sub 폴더 아래에 새로운 임시 폴더를 생성하세요.
+//	폴더명은 UUID를 활용하여 구성하세요.
+//	단, 폴더명에 -(하이픈)은 없어야 합니다.
+	
+//	[출력]
+//	임시 폴더 생성: C:\test\4e9719b5f849425cb62cfc2711772a6b
+	
+	public static void main(String[] args) {
+		String dirPath = "C:\\test\\sub";
+		String dirName = UUID.randomUUID().toString().replaceAll("-", "");
+		File uploadDir = new File(dirPath + "\\" + dirName);
+		
+		// 폴더가 없으면 생성
+		if (!uploadDir.exists()) {
+			boolean dirCreated = uploadDir.mkdirs();
+			if (!dirCreated) {
+				System.out.println("폴더 생성 실패");
+				return;
+			}
+			System.out.println("폴더 생성 성공: " + dirPath + "\\" + dirName);
+		}
+		
+	}
+
+}
